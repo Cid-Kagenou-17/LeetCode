6ac0071296529097f55dbeab72e3fd56999dbd82
Cid <dev.debojyoti17@gmail.com>
2025-05-19 09:09:02 +0530

    Time: 0 ms (100%), Space: 23.7 MB (78.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-        int x=0;
-
+        int x =-1;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                nums[x] = nums[i];
-                x++;
+                nums[++x]=nums[i];
             }
         }
-        for(int i=x;i<nums.size();i++){
-            nums[i]=0;
+        if(x!=-1){
+            for(int i=x+1;i<nums.size();i++){
+                nums[i]=0;
+            }
         }
-
-        
         
     }
 };
