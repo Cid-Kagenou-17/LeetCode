06869fe32ae8acaa5e1b479ff81fa3a5473d89dc
Cid <dev.debojyoti17@gmail.com>
2025-05-19 09:02:52 +0530

    Time: 0 ms (100%), Space: 11.3 MB (45.44%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int c=0,m=0;
-        int i=0,j=0;
+
+        int i=0,j=0,c=0;
         int N = nums.size();
+
         if(nums[0]>=nums[N-1]){
-
             for(i=1;i<N;i++){
-                if(nums[i]>=nums[i-1])
+                if(nums[i]>=nums[i-1]){
                     c++;
-                else
+                }
+                else{
                     break;
+                }
             }
-
-            for(j =c+1;j<N-1;j++){
-                if(nums[j]>nums[j+1])
+            for(j = c+1;j<N-1;j++){
+                if(nums[j+1]<nums[j]){
                     break;
+                }
             }
-
-            if(i==N || j==N-1)
+            if(j==N-1 || i==N)
             return true;
             else
             return false;
         }
         else{
             for(int i=1;i<N;i++){
-
-                if(nums[i]<nums[i-1])
-                    return false;
+                if(nums[i]>=nums[i-1]){
+                    c++;
+                }
+                else{
+                    break;
+                }
             }
+            if(c==N-1)
             return true;
+            else
+            return false;
         }
+
         
     }
 };
