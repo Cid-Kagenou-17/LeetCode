666fdc8bb0e0dc6f489eec330c3b70cce7c0eeca
Cid <dev.debojyoti17@gmail.com>
2025-05-20 15:41:08 +0530

    Time: 0 ms (100%), Space: 30.5 MB (16.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-
+        
         int N = nums.size();
+        pair <int,int> p_array[N];
 
-            pair <int,int> p_array[N];
+        for(int i=0;i<N;i++){
 
-            for(int i=0;i<N;i++){
-                p_array[i].first = i;
-            }
+            p_array[i].first = (i+k)%N;
+            p_array[i].second = nums[i];
 
-            for(int i=0;i<N;i++){
-                p_array[i].second = nums[i];
-            }
-
-            for(int i=0;i<N;i++){
-                int pi = p_array[i].first;
-                int newindex = (pi+k)%N;
-                p_array[i].first = newindex; 
-            }
-
-            for(int i=0;i<N;i++){
-                nums[p_array[i].first] = p_array[i].second;
-            }
         }
+ 
+        for(int i=0;i<N;i++){
+            nums[p_array[i].first] = p_array[i].second;
+        }
+
+        
+
+        
+    }
 };
