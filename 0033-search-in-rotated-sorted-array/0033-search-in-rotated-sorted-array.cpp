e6babea73ef552c3baf25c49725fb1e2b16ca9cf
Cid <dev.debojyoti17@gmail.com>
2025-06-05 16:42:09 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,80 +1,58 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int st = 0;
-        int end = nums.size() - 1;
-        int N = nums.size();
-        int startindex = 0;
+    int search(vector<int>& arr, int target) {
+        
+        int ans;
+        int st=0;
+        int N = arr.size();
+        int end = N-1;
+        if(arr[0]<=arr[N-1]){
+            ans =  0;
+        }
+        else{
+        
+        while(st<=end){
+            
+            int mid = (st + (end-st)/2);
+            
+            if(arr[mid]<=arr[(mid-1+N)%N] && arr[mid]<=arr[(mid+1+N)%N]){
+            ans =  mid;
+            break;}
+            else if(arr[mid]>=arr[0])
+            st = mid+1;
+            else
+            end = mid-1;
+        }
+        }
+        st = 0;
+        end = ans-1;
+        while(st<=end){
 
-        while (st <= end) {
+            int mid = (st + (end-st)/2);
 
-            int mid = (st + end) / 2;
-
-            if (nums[mid] <= nums[(mid - 1 + N) % N] &&
-                nums[mid] <= nums[(mid + 1) % N]) {
-                startindex = mid;
-                break;
-            }
-
-            else if (nums[mid] <= nums[end])
-                end = mid - 1;
-
-
-            else if (nums[mid] >= nums[st])
-                st = mid + 1;
-
+            if(arr[mid]==target)
+            return mid;
+            else if(arr[mid]<=target)
+            st = mid+1;
+            else if(arr[mid]>=target)
+            end = mid-1;
         }
 
-        if (startindex != 0) {
+        st = ans;
+        end = N-1;
+        while(st<=end){
 
-            st = 0;
-            end = startindex - 1;
+            int mid = (st + (end-st)/2);
 
-            while (st <= end) {
-
-                int mid = (st + end) / 2;
-
-                if (nums[mid] == target)
-                    return mid;
-                else if (nums[mid] > target)
-                    end = mid - 1;
-                else if (nums[mid] < target)
-                    st = mid + 1;
-            }
-
-            st = startindex;
-            end = nums.size() - 1;
-
-            while (st <= end) {
-
-                int mid = (st + end) / 2;
-
-                if (nums[mid] == target)
-                    return mid;
-                else if (nums[mid] > target)
-                    end = mid - 1;
-                else if (nums[mid] < target)
-                    st = mid + 1;
-            }
-        }
-
-        else {
-
-            st = 0;
-            end = nums.size() - 1;
-
-            while (st <= end) {
-
-                int mid = (st + end) / 2;
-
-                if (nums[mid] == target)
-                    return mid;
-                else if (nums[mid] > target)
-                    end = mid - 1;
-                else if (nums[mid] < target)
-                    st = mid + 1;
-            }
+            if(arr[mid]==target)
+            return mid;
+            else if(arr[mid]<=target)
+            st = mid+1;
+            else if(arr[mid]>=target)
+            end = mid-1;
         }
         return -1;
+
+        
     }
 };
