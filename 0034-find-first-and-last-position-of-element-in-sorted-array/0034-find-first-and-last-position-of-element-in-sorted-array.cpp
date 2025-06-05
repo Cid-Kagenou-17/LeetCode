cfbe3310e4feab77059e224995a64dea63ed812c
Cid <dev.debojyoti17@gmail.com>
2025-06-05 12:12:48 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-
-        int startindex = -1,endindex = -1;
-        int st=0,end = nums.size()-1;
-
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int st = 0;
+        int end = arr.size()-1;
+        int firstoccurence = -1;
+        int lastoccurence = -1;
+        
         while(st<=end){
-
-            int mid = (st+end)/2;
-
-            if(nums[mid]==target){
-                startindex = mid;
+            
+            int mid = (st + (end-st)/2);
+            if(arr[mid]==target){
+                firstoccurence = mid;
                 end = mid-1;
             }
-            else if(nums[mid]>target){
+            else if(arr[mid]>target){
                 end = mid-1;
             }
-
-            else if(nums[mid]<target)
-            st = mid+1;
-        }
-        st =0;
-        end = nums.size()-1;
-
-        while(st<=end){
-
-            int mid = (st+end)/2;
-
-            if(nums[mid]==target){
-                endindex = mid;
+            else if(arr[mid]<target){
                 st = mid+1;
             }
-            else if(nums[mid]>target){
+        }
+        
+        st = 0;
+        end= arr.size()-1;
+        
+        while(st<=end){
+            
+            int mid = (st + (end-st)/2);
+            if(arr[mid]==target){
+                lastoccurence = mid;
+                st = mid+1;
+            }
+            else if(arr[mid]>target){
                 end = mid-1;
             }
-
-            else if(nums[mid]<target)
-            st = mid+1;
-
+            else if(arr[mid]<target){
+                st = mid+1;
+            }
         }
 
-        return {startindex, endindex};
+        return {firstoccurence,lastoccurence};
+
+
         
     }
 };
