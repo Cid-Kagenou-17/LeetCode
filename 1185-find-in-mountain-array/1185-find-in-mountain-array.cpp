30ceedee66fea26904f9f7929e853ed8594debd5
Cid <dev.debojyoti17@gmail.com>
2025-06-05 13:25:18 +0530

    Time: 3 ms (75.72%), Space: 9.7 MB (54.92%) - LeetHub

--- 1185-find-in-mountain-array/1185-find-in-mountain-array.cpp
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.cpp
@@ -11,48 +11,48 @@
 class Solution {
 public:
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        int length = mountainArr.length();
+        int n = mountainArr.length();
 
-        int left = 0, right = length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+      
+        int st = 1, end = n - 2, index = -1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
             int midVal = mountainArr.get(mid);
-            int nextVal = mountainArr.get(mid + 1);
+            int leftVal = mountainArr.get(mid - 1);
+            int rightVal = mountainArr.get(mid + 1);
 
-            if (midVal < nextVal)
-                left = mid + 1;
-            else
-                right = mid;
+            if (midVal > leftVal && midVal > rightVal) {
+                index = mid;
+                break;
+            } else if (midVal < rightVal) {
+                st = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
-        int peak = left;
+
+        if (index == -1) return -1;
 
         
-        left = 0;
-        right = peak;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int midVal = mountainArr.get(mid);
-
-            if (midVal == target)
-                return mid;
-            else if (midVal < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
+        st = 0;
+        end = index;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            else if (val < target) st = mid + 1;
+            else end = mid - 1;
         }
-        
-        left = peak + 1;
-        right = length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int midVal = mountainArr.get(mid);
 
-            if (midVal == target)
-                return mid;
-            else if (midVal < target)
-                right = mid - 1;
-            else
-                left = mid + 1;
+     
+        st = index + 1;
+        end = n - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            int val = mountainArr.get(mid);
+            if (val == target) return mid;
+            else if (val > target) st = mid + 1;
+            else end = mid - 1;
         }
 
         return -1;
